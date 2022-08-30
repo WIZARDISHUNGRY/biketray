7fa0883f8b2795541da96a70404349153af85e5e
Jon Williams <jon.williams@thetalake.com>
2022-08-30 12:42:13 -0400

    open apple maps

--- links/location.go
+++ links/location.go
@@ -2,6 +2,7 @@ package links
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/skratchdot/open-golang/open"
 	"jonwillia.ms/biketray/geo"
@@ -12,10 +13,19 @@ import (
 // https://www.google.com/maps?saddr=My+Location&daddr=43.12345,-76.12345
 
 func OpenLocation(startLoc *geo.LocationInfo, loc geo.LocationInfo) error {
+	host := "www.google.com"
+	var isApple = false
+	if runtime.GOOS == "darwin" {
+		host = "maps.apple.com"
+		isApple = true
+	}
 	startStr := "My+Location"
 	if startLoc != nil {
 		startStr = fmt.Sprintf("%f,%f", startLoc.Lat, startLoc.Lon)
 	}
-	s := fmt.Sprintf("https://www.google.com/maps?saddr=%s&daddr=%f,%f", startStr, loc.Lat, loc.Lon)
+	s := fmt.Sprintf("https://%s/maps?saddr=%s&daddr=%f,%f", host, startStr, loc.Lat, loc.Lon)
+	if isApple {
+		return open.StartWith(s, "Maps.App")
+	}
 	return open.Start(s)
 }
